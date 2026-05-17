6ebf45eec79e73f61bb6f47c63dab47fb4014c63
ZeMove228 <azzzzamov@gmail.com>
2026-05-17 13:56:54 +0500

    skip check for second channel

--- config.py
+++ config.py
@@ -1,4 +1,4 @@
-BOT_TOKEN = "8988853545:AAFyCWL8ve15MPMR3yZKtQfLNovtgnS5lX0"
+BOT_TOKEN = "8479106195:AAEDmiGYtO1a-yuCZ68yboWI0wbfnkdDRJ8"
 
 REGISTRATION_LINK = "https://new-form-project-37c3ad.zapier.app/cau-international-olympiad-2026"
 
